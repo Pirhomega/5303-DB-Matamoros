4ab0db592a3fa7ccffd182871c9979ad9b33b598
Pirhomega <35641464+Pirhomega@users.noreply.github.com>
2019-10-16 11:16:52 -0500

    finished initial project

--- A7/plotting/airports.py
+++ A7/plotting/airports.py
@@ -10,18 +10,18 @@ mapbox_token = ""
 # using pymongo to interface with my mongo database image (mongodb) in docker
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["armageddon"]
-uf = db["ufos"]
+ap = db["airports"]
 
 # iterate through mongodb collection and create a pandas dataframe with the collection data
-cursor_ufos = uf.find()
-df = pd.DataFrame(list(cursor_ufos))
+cursor_airport = ap.find()
+df = pd.DataFrame(list(cursor_airport))
 
 # sets the geospatial data up
 figure = go.Figure(go.Scattermapbox(lat = df["latitude"], 
                                     lon = df["longitude"],
-                                    text = df["city"],
+                                    text = df["airport"],
                                     mode = "markers",   
-                                    marker=dict(size=4,color="blue",opacity=.8)
+                                    marker=dict(size=4,color="green",opacity=.8)
                                     )
                                 )
 
@@ -34,7 +34,7 @@ figure.update_layout(
                                 center = go.layout.mapbox.Center(lat = 33.9137, lon = -98.4934),
                                 pitch = 0,
                                 zoom = 5),
-    title="UFO sightings around the world")
+    title="Airports around the world")
 
 # displays the data using the default renderer
 figure.show()
--- A7/plotting/earthquakes.py
+++ A7/plotting/earthquakes.py
@@ -10,18 +10,18 @@ mapbox_token = ""
 # using pymongo to interface with my mongo database image (mongodb) in docker
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["armageddon"]
-uf = db["ufos"]
+eq = db["earthquakes"]
 
 # iterate through mongodb collection and create a pandas dataframe with the collection data
-cursor_ufos = uf.find()
-df = pd.DataFrame(list(cursor_ufos))
+cursor_earthquake = eq.find()
+df = pd.DataFrame(list(cursor_earthquake))
 
 # sets the geospatial data up
 figure = go.Figure(go.Scattermapbox(lat = df["latitude"], 
                                     lon = df["longitude"],
-                                    text = df["city"],
+                                    text = df["place"],
                                     mode = "markers",   
-                                    marker=dict(size=4,color="blue",opacity=.8)
+                                    marker=dict(size=4,color="orange",opacity=.8)
                                     )
                                 )
 
@@ -34,7 +34,7 @@ figure.update_layout(
                                 center = go.layout.mapbox.Center(lat = 33.9137, lon = -98.4934),
                                 pitch = 0,
                                 zoom = 5),
-    title="UFO sightings around the world")
+    title="Airports around the world")
 
 # displays the data using the default renderer
 figure.show()
