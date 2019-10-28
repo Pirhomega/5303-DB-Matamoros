75732cbd03485abf71089cc7fb38d6c4812d331c
Pirhomega <35641464+Pirhomega@users.noreply.github.com>
2019-10-28 17:41:15 -0500

    messed with python couchbase SDK

--- couchbase_presentation/test1.py
+++ couchbase_presentation/test1.py
@@ -1,10 +1,14 @@
 from couchbase.cluster import Cluster
 from couchbase.cluster import PasswordAuthenticator
+from couchbase.n1ql import N1QLQuery
 
 cluster = Cluster('couchbase://localhost:8091')
 authenticator = PasswordAuthenticator('corbinmatamoros', 'Matt&Corbin')
 cluster.authenticate(authenticator)
+
+# opens whatever bucket
 cb = cluster.open_bucket('swallows-sample')
+
 cb.upsert('u:king_arthur', {'name': 'Arthur', 'email': 'kingarthur@couchbase.com', 'interests': ['Holy Grail', 'African Swallows']})
 # OperationResult<RC=0x0, Key=u'u:king_arthur', CAS=0xb1da029b0000>
 
@@ -13,8 +17,8 @@ cb.get('u:king_arthur').value
 
 ## The CREATE PRIMARY INDEX step is only needed the first time you run this script
 cb.n1ql_query('CREATE PRIMARY INDEX ON `swallows-sample`').execute()
-from couchbase.n1ql import N1QLQuery
 row_iter = cb.n1ql_query(N1QLQuery('SELECT name FROM `swallows-sample` WHERE ' +\
 '$1 IN interests', 'African Swallows'))
-for row in row_iter: print(row)
+for row in row_iter: 
+  print(row)
 # {u'name': u'Arthur'}
