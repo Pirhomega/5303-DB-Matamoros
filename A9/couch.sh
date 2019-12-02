0c93da33a03d42d99249441d1e9a5f1c3ce88f9b
Corbin Matamoros <35641464+Pirhomega@users.noreply.github.com>
2019-12-02 14:51:10 -0600

    added installation file

---
+++ A9/couch.sh
@@ -0,0 +1,13 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install python3
+brew cask install couchbase-server-enterprise
+python3 -m pip install --upgrade pip
+python3 -m pip install markovify
+brew install libcouchbase
+python3 -m pip install couchbase
+ping 10.0.88.237 -c 3
+brew install wget
+wget https://www.gutenberg.org/files/2591/2591-0.txt -O 1.txt
+wget http://www.gutenberg.org/cache/epub/5200/pg5200.txt -O 2.txt
+wget https://www.gutenberg.org/files/16/16-0.txt -O 3.txt
+wget https://www.gutenberg.org/files/1184/1184-0.txt -O 4.txt
