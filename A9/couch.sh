8a00785955fffc93f69591affbf90cbaba4895a0
Corbin Matamoros <35641464+Pirhomega@users.noreply.github.com>
2019-12-09 09:42:54 -0600

    fixed incorrect stuff and added coments

--- A9/couch.sh
+++ A9/couch.sh
@@ -1,12 +1,24 @@
+# install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# install python 3.7.5
 brew install python3
+brew upgrade python3
+# install a Couchbase instance. Not necessary to just upload messages.
 brew cask install couchbase-server-enterprise
-python3 -m pip install --upgrade pip
-python3 -m pip install markovify
+# upgrades pip
+pip3 install --upgrade pip
+# installs markovify, which will generate random sentences
+pip3 install markovify
+# installs the library necessary for Couchbase
 brew install libcouchbase
-python3 -m pip install couchbase
-ping  -c 3
+# installs the Couchbase python SDK
+pip3 install couchbase
+# pings the computer you want to upload messages to 
+ping <IP address> -c 3
+# installs and upgrades wget, which will grab text from the web to train markovify
 brew install wget
+brew upgrade wget
+# grab text from the web and renames (-O <renamed files>)
 wget https://www.gutenberg.org/files/2591/2591-0.txt -O 1.txt
 wget http://www.gutenberg.org/cache/epub/5200/pg5200.txt -O 2.txt
 wget https://www.gutenberg.org/files/16/16-0.txt -O 3.txt
